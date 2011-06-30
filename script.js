c15d338901d62ed6b62eb3960506e7b77d717ca7
Gregory Crosswhite <gcrosswhite@gmail.com>
2011-06-30 13:22:37 -0700

    Created title slide and background for the talk!

---
+++ script.js
@@ -0,0 +1,22 @@
+//@+leo-ver=5-thin
+//@+node:gcross.20110629122941.1128: * @file script.js
+//@@language javascript
+
+window.addEventListener("load",function() {
+    initializeSlick([
+        //@+<< Script >>
+        //@+node:gcross.20110629122941.1129: ** << Script >>
+        hireUseActor("title"),
+        "outline",
+        hireUseActor("standard_backdrop","title"),
+        hireUseActor("grey_rectangle","title"),
+        parallel(
+            fadeOut(1,function(stage) { return stage.grey_rectangle; }),
+            fadeOut(1,function(stage) { return stage.title; })
+        ),
+        fire("title"),
+        fire("grey_rectangle"),
+        //@-<< Script >>
+    ])
+},false)
+//@-leo
